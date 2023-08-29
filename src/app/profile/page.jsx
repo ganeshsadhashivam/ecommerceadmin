0eba1eda63936bad88bda8953363db3005e5e221
ganesh <ganeshsadhashivam@outlook.com>
2023-08-29 19:58:35 +0530

    logoutfunctionalitydone

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,30 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
 const Profile = () => {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("logout success");
+      router.push("/login");
+    } catch (error) {
+      console.log(error.message);
+
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="bg-black flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-white">Profile</h1>
+      <button
+        onClick={logout}
+        className="bg-blue-700 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+      >
+        logout
+      </button>
     </div>
   );
 };
